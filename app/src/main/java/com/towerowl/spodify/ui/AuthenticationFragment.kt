aec121589a359f156ed3b7668e4ccf63978dceda
Hannes <h.haggander@gmail.com>
2020-08-17 21:31:00 +0200

    add check for spotify installed on device

--- app/src/main/java/com/towerowl/spodify/ui/AuthenticationFragment.kt
+++ app/src/main/java/com/towerowl/spodify/ui/AuthenticationFragment.kt
@@ -1,13 +1,22 @@
 package com.towerowl.spodify.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.towerowl.spodify.R
+import kotlinx.android.synthetic.main.fragment_authentication.*
 
 class AuthenticationFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "AuthenticationFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +26,39 @@ class AuthenticationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (!SpotifyAppRemote.isSpotifyInstalled(requireContext())) {
+            authentication_message.text = getString(
+                R.string.error_spotify_not_installed,
+                getString(R.string.app_name)
+            )
+        } else {
+            connectToSpotify()
+        }
+
+    }
+
+    private fun connectToSpotify() {
+        ConnectionParams.Builder(getString(R.string.spotify_client_id))
+            .showAuthView(true)
+            .build()
+            .run {
+                SpotifyAppRemote.connect(
+                    requireContext(),
+                    this,
+                    object : Connector.ConnectionListener {
+                        override fun onFailure(error: Throwable?) {
+                            Log.w(TAG, "onFailure: Failed to connect", error)
+
+                        }
+
+                        override fun onConnected(spotifyAppRemote: SpotifyAppRemote?) {
+                            Log.d(TAG, "onConnected: Connected!")
+                        }
+
+                    })
+            }
+
+
     }
 }
