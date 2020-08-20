623fa77240d306b72a4d40b291dfc30d0040fa07
Hannes <h.haggander@gmail.com>
2020-08-20 23:14:27 +0200

    update show layout design

--- app/src/main/java/com/towerowl/spodify/ui/HomeFragment.kt
+++ app/src/main/java/com/towerowl/spodify/ui/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -69,8 +70,9 @@ class HomeFragment : Fragment() {
     private fun setupRecycler() {
         with(home_show_recycler) {
             adapter = showRecyclerAdapter
-            layoutManager = LinearLayoutManager(
+            layoutManager = GridLayoutManager(
                 requireContext(),
+                2,
                 LinearLayoutManager.VERTICAL,
                 false
             )
@@ -106,7 +108,7 @@ class ShowRecyclerAdapter(val onClick: (ShowDataItem) -> Unit) :
                 .load(show.images.first().url)
                 .into(holder.itemView.show_item_image)
             holder.itemView.show_item_name.text = show.name
-            holder.itemView.show_item_description.text = show.description
+            holder.itemView.show_item_publisher.text = show.publisher
             holder.itemView.setOnClickListener { onClick(this) }
         }
     }
