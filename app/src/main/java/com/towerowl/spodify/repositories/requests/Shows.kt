ae02b2e4e11dcaef960018bc21583c67b0b6ba3d
Hannes <h.haggander@gmail.com>
2020-08-21 00:07:49 +0200

    [wip] prepare query parameters for shows call

--- app/src/main/java/com/towerowl/spodify/repositories/requests/Shows.kt
+++ app/src/main/java/com/towerowl/spodify/repositories/requests/Shows.kt
@@ -4,8 +4,17 @@ import com.towerowl.spodify.data.api.ShowEpisodes
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 interface Shows {
     @GET("shows/{id}/episodes")
-    fun shows(@Path("id") showId: String): Call<ShowEpisodes>
+    fun shows(
+        @Path("id") showId: String,
+        @QueryMap optional: Map<String, String> = mapOf()
+    ): Call<ShowEpisodes>
+
+    companion object {
+        const val QUERY_OFFSET = "offset"
+        const val QUERY_LIMIT = "limit"
+    }
 }
